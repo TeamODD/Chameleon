4341d889b70e3b817de9fc35f89cea09fdfe4962
mandugii <dbalsgur12345@gmail.com>
2025-11-09 02:02:06 +0900

    Add SFX

--- Assets/YMH/Scripts/PlaySFX.cs
+++ Assets/YMH/Scripts/PlaySFX.cs
@@ -1,16 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlaySFX : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public AudioClip sfxClip;
+
+
     void Start()
     {
-        
+       
+    }
+    public void OnClicked()
+    {
+        if (AudioManager.Instance == null)
+        {
+            Debug.LogError("AudioManager instance not found!");
+            return;
+        }
+
+        if (sfxClip == null)
+        {
+            Debug.LogError("No SFX clip assigned!");
+            return;
+        }
+
+        AudioManager.Instance.PlaySFX(sfxClip);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
