850ed66de542fa95d37af35221f53ce03d2408f6
mandugii <dbalsgur12345@gmail.com>
2025-11-09 20:55:10 +0900

    asdfasdf

---
+++ Assets/YMH/Scripts/Mousefix.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Mousefix : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("dasjkg");
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
